cf1570bd55d56357e7b13ff9a679213789c941a7
Karl Southern <karl@theangryangel.co.uk>
2020-09-17 13:21:35 +0100

    Initial commit

---
+++ website_robots/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "website_robots",
+    "summary": "Module summary",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://www.glodo.uk/",
+    "author": "GlodoUK",
+    "application": True,
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/robots.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
