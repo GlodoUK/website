a9f3723f8469dba1b49b54b32a9c09192eb60262
Karl Southern <karl@theangryangel.co.uk>
2020-11-02 10:46:16 +0000

    Add 14.0

---
+++ website_leaflet/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "website_leaflet",
+    "summary": "Adds a leaflet.js powered map (eventually snippet)",
+    "author": "Glodo",
+    "website": "https://www.glodo.uk/",
+    "category": "Hidden",
+    "version": "13.0.1.0.0",
+    "depends": ["website"],
+    "external_dependencies": {"python": []},
+    "data": [
+        "templates/assets.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "license": "AGPL-3",
+}
